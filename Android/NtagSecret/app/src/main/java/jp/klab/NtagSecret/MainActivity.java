5908c5551e69fddcf74e73aa6455172ad0ae5fab
TANABE Makoto <mkttanabe2012@gmail.com>
2017-12-26 18:01:52 +0900

    copyright

--- Android/NtagSecret/app/src/main/java/jp/klab/NtagSecret/MainActivity.java
+++ Android/NtagSecret/app/src/main/java/jp/klab/NtagSecret/MainActivity.java
@@ -8,7 +8,21 @@
       パスワードとして指定された文字列の CRC32 値を格納する
     - NTAG への上記の Write 機能に加え Read機能, パスワード解除＆データ初期化機能をもつ
 
-    2017.12
+   2017.12
+
+   Copyright 2017 KLab Inc.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
  */
 
 package jp.klab.NtagSecret;
